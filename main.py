651f3b5e3934292b522ba75aa09e6dd38d95116f
MelnikovAndre <melnikov2010@yandex.ru>
2024-10-01 20:51:07 +0300

    "Базовые структуры данных"

---
+++ main.py
@@ -0,0 +1,19 @@
+#"1st program"
+print((9**0.5)*5)
+
+#"2st program"
+print( 9.99 > 9.98 and 1000 != 1000.1)
+
+#"3rd program"
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2) == (2*(2+2)))
+
+#"4th program"
+a = '123.456'
+a = float(a)
+a = a*10.0
+a = int(a)
+a = a % 10
+print(a)
+
