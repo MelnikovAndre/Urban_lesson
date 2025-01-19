17aa6e4f637dfaf9200323d5f6fbf7fb0137ee14
MelnikovAndre <melnikov2010@yandex.ru>
2025-01-19 14:11:08 +0300

    Домашнее задание по теме "Виртуальные окружения"

--- main.py
+++ main.py
@@ -1,19 +1,48 @@
-#"1st program"
-print((9**0.5)*5)
+import numpy as np
+import pygame as pg
 
-#"2st program"
-print( 9.99 > 9.98 and 1000 != 1000.1)
 
-#"3rd program"
-print(2*2+2)
-print(2*(2+2))
-print((2*2+2) == (2*(2+2)))
+fps = pg.time.Clock()
+screen = pg.display.set_mode((510, 510))
+map_colors = np.arange(0, 510)
+for i in map_colors:
+    for j in map_colors:
+        surf = pg.Surface((1, 1))
+        surf.fill((i // 2, i // 2, i // 2))
+        screen.blit(surf, (i, j))
 
-#"4th program"
-a = '123.456'
-a = float(a)
-a = a*10.0
-a = int(a)
-a = a % 10
-print(a)
+r = 0
+g = 0
+b = 255
+circle_start_size = 1
+circle_end_size = 50
+current_size = circle_end_size
+size_dir = 1
+
+while True:
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            exit()
+    if pg.mouse.get_pressed()[0]:
+        pg.draw.circle(screen, (r, g, b), pg.mouse.get_pos(), current_size, 2)
+
+    if r < 255 and g == 0:
+        r += 1
+        b -= 1
+    elif g < 255 and b == 0:
+        g += 1
+        r -= 1
+    else:
+        b += 1
+        g -= 1
+
+    if current_size in (circle_start_size, circle_end_size):
+        size_dir *= -1
+
+    current_size += size_dir
+
+    fps.tick(60)
+    pg.display.update()
+
 
